6d110a1a734b27fd7b4b6cc1ae68dedc4dff689a
eel-primo <47497416+eel-primo@users.noreply.github.com>
2022-04-09 14:44:28 +0300

    Resaved

--- example.py
+++ example.py
@@ -1,1 +1,16 @@
-from pyAutoUpdate import lib_installer, file_updaterli = lib_installer(['colorama'])fu = file_updater(['example.py'], ['https://raw.githubusercontent.com/eel-primo/pyAutoUpdate/main/example.py'])li.install()from colorama import Fore#Sadly, but works fine only on Linuxif fu.install():    print(Fore.YELLOW + "Files were updated, reopen app for applying changes")    exit()#Your codeprint(Fore.GREEN + "Hello world!")input()
+from pyAutoUpdate import lib_installer, file_updater
+
+li = lib_installer(['colorama'])
+fu = file_updater(['example.py'], ['https://raw.githubusercontent.com/eel-primo/pyAutoUpdate/main/example.py'])
+
+li.install()
+from colorama import Fore
+
+#Sadly, but works fine only on Linux :(
+if fu.install():
+    print(Fore.YELLOW + "Files were updated, reopen app for applying changes")
+    exit()
+
+#Your code
+print(Fore.GREEN + "Hello world!")
+input()
